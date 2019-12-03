064912af06a688925217fe4def37e60a88f31a9c
Tyler <tylerhanson921@gmail.com>
2019-12-02 19:58:09 -0600

    added dynamic image size to evaluate function

--- modules/evaluate.py
+++ modules/evaluate.py
@@ -3,7 +3,7 @@ import os
 import numpy as np
 import gym
 
-
+from environments.gym import OpenAIGym
 from modules.network import FeatureDetector
 from modules.run import Run
 from modules.policy import Policy
@@ -15,11 +15,20 @@ from parameters import Hp
 def evaluate(n_steps):
     hp = Hp()
     np.random.seed(hp.seed)
-    env = gym.make(hp.env_name)
+    env = OpenAIGym(hp.env_name)
     #env = wrappers.Monitor(env, monitor_dir, force=True)
     nb_inputs = env.observation_space.shape[0]
     if hp.conv_input:
-        nb_inputs = 2304  # inputs after convolution TODO build function that finds output for cnn
+        # create dummy image
+        test_img = np.ones(
+            [env.observation_space.shape[0], env.observation_space.shape[1], env.observation_space.shape[2]]).astype(
+            np.uint8)
+        # create dummy Normalizer obj
+        test_n = Normalizer(0, hp)
+        # pass through cnn
+        test_output = test_n.image_cnn(test_img)
+        # get output length
+        nb_inputs = len(test_output)
     nb_outputs = env.action_space.shape[0]
     policy = Policy(nb_inputs, nb_outputs, hp)
     normalizer = Normalizer(nb_inputs, hp)
